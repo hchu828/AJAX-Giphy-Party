739e6cae0544bd3ae3e9f0220201da9c644ac463
hectorchu~ <hector.chu828@gmail.com>
2022-01-18 16:25:44 -0800

    Added getGif function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Let's get this party started!");
+
+async function getGif(event) {
+    event.preventDefault();
+    const formVal = document.getElementById("form-giphy").value;
+    const giphyArray = await axios.get("https://api.giphy.com/v1/gifs/search", {
+        params: {
+            api_key: "BiueiEqDTA5iIEyGTAgmUhRBAy4f8rSb",
+            q: formVal
+        }
+    });
+    console.log(giphyArray.data);
+}
+
+document.getElementById("submit").addEventListener("click", getGif);
