072c2cabed4a32381d37d73ef8df3816e499ac6c
pixelready <bizartist@gmail.com>
2022-01-18 17:44:42 -0800

    better variable names and now it's purty

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-"use strict";
+'use strict';
 
 console.log("Let's get this party started!");
 
 /**
  * take form value and pass to Giphy Search Endpoint
  * append gif to gif container
- * @param {event} event 
+ * @param {event} event
  */
-async function getGif(event) {
-    event.preventDefault();
-    //TODO: formVal -> searchTermVal
-    const formVal = document.getElementById('form-giphy').value;
-    //TODO: giphyArray -> response 
-    const giphyArray = await axios.get('https://api.giphy.com/v1/gifs/search', {
-        params: {
-            api_key: 'BiueiEqDTA5iIEyGTAgmUhRBAy4f8rSb',
-            q: formVal,
-            limit: 1
-        }
-    });
-    console.log(giphyArray);
+async function getGif (event) {
+  event.preventDefault();
 
-    const newGif = giphyArray.data.data[0];
-    //TODO: make gifURL its own variable
-    const newGifHtml = `<div class="funny-gif"><img src='${newGif.images.original.url}' width='200px' /></div>`;
-    $('#gif-container').append(newGifHtml);
+  const searchTermVal = $('#search-term-input').val();
+  const giphyResponse = await axios.get(
+    'https://api.giphy.com/v1/gifs/search',
+    {
+      params: {
+        api_key: 'BiueiEqDTA5iIEyGTAgmUhRBAy4f8rSb',
+        q: searchTermVal,
+        limit: 1
+      }
+    }
+  );
+
+  console.log(giphyResponse);
+
+  const newGifObj = giphyResponse.data.data[0];
+  const newGifUrl = newGifObj.images.original.url;
+
+  const newGifHtml = `<div class="funny-gif"><img src='${newGifUrl}' width='200px' /></div>`;
+  $('#gif-container').append(newGifHtml);
 }
 
 /**
  * empty gif-container
  */
-function removeImages() {
-    $("gif-container").empty();
+function removeImages () {
+  $('gif-container').empty();
 }
 //TODO: submit event
-document.getElementById('submit').addEventListener('click', getGif);
+$('#search-form').on('submit', getGif);
 
-$("#remove").on("click", removeImages);
+$('#remove').on('click', removeImages);
